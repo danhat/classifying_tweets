357d85cb480a5b828f7828bdd3e436b7be1420e8
Danielle Hatten <danhat@users.noreply.github.com>
2021-02-23 19:40:48 -0600

    changing markdown boxes

--- tweets.py
+++ tweets.py
@@ -4,11 +4,12 @@ import pandas as pd
 import nltk
 import sklearn 
 import string
-import re # helps you filter urls
+import re # helps  filter urls
 from sklearn.metrics import accuracy_score
 #from IPython.display import display, Latex, Markdown
 import copy
 from scipy import stats
+
 
 
 nltk.download('stopwords')
@@ -23,11 +24,6 @@ stopwords = nltk.corpus.stopwords.words('english')
 
 
 
-
-#Whether to test your Q9 for not? Depends on correctness of all modules
-def test_pipeline():
-	return False # Make this true when all tests pass
-
 posMapping = {
     # "First_Letter by nltk.pos_tag":"POS_for_lemmatizer"
     "N":'n',
@@ -36,15 +32,18 @@ posMapping = {
     "R":'r'
   }
 
-def process(text, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
-  """ Normalizes case and handles punctuation
+
+""" 
+  Normalizes case and handles punctuation
   Inputs:
     text: str: raw text
     lemmatizer: an instance of a class implementing the lemmatize() method
                 (the default argument is of type nltk.stem.wordnet.WordNetLemmatizer)
   Outputs:
     list(str): tokenized text
-  """
+"""
+def process(text, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
+  
   
   text = text.lower()
   text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text) # remove url
@@ -77,9 +76,8 @@ def process(text, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
 
 
 
-
-def process_all(df, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
-  """ process all text in the dataframe using process function.
+""" 
+  process all text in the dataframe using process function.
   Inputs
     df: pd.DataFrame: dataframe containing a column 'text' loaded from the CSV file
     lemmatizer: an instance of a class implementing the lemmatize() method
@@ -87,16 +85,15 @@ def process_all(df, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
   Outputs
     pd.DataFrame: dataframe in which the values of text column have been changed from str to list(str),
                 the output from process_text() function. Other columns are unaffected.
-  """
- 
+"""
+def process_all(df, lemmatizer = nltk.stem.wordnet.WordNetLemmatizer()):
   df['text'] = df['text'].apply(process)
   return df
     
 
 
-
-def create_features(processed_tweets, stop_words):
-  """ creates the feature matrix using the processed tweet text
+""" 
+  creates the feature matrix using the processed tweet text
   Inputs:
     tweets: pd.DataFrame: tweets read from train/test csv file, containing the column 'text'
     stop_words: list(str): stop_words by nltk stopwords
@@ -104,8 +101,8 @@ def create_features(processed_tweets, stop_words):
     sklearn.feature_extraction.text.TfidfVectorizer: the TfidfVectorizer object used
         we need this to tranform test tweets in the same way as train tweets
     scipy.sparse.csr.csr_matrix: sparse bag-of-words TF-IDF feature matrix
-  """
-  
+"""
+def create_features(processed_tweets, stop_words):  
   docs2 = copy.deepcopy(processed_tweets['text'])
   docs = [' '.join(i) for i in docs2]
 
@@ -117,15 +114,14 @@ def create_features(processed_tweets, stop_words):
 
 
 
-
-def create_labels(processed_tweets):
-  """ creates the class labels from screen_name
+""" 
+  creates the class labels from screen_name
   Inputs:
     tweets: pd.DataFrame: tweets read from train file, containing the column 'screen_name'
   Outputs:
     numpy.ndarray(int): dense binary numpy array of class labels
   """
-  
+def create_labels(processed_tweets): 
   labels = []
   initialize = 1
   for user in processed_tweets['screen_name']:
@@ -146,33 +142,24 @@ def create_labels(processed_tweets):
 
 
 
-
-class MajorityLabelClassifier():
-  """
+"""
   A classifier that predicts the mode of training labels
-  """
+"""
+class MajorityLabelClassifier():
+
+  # initializa
   def __init__(self):
-    """
-    Initialize
-    """
     self.mode = None
-    
-        
+  
+
+  # Implement fit by taking training data X and their labels y and finding the mode of y  
   def fit(self, X, y):
-    """
-    Implement fit by taking training data X and their labels y and finding the mode of y
-    """
-    
     m = stats.mode(y)
     self.mode = int(m[0])
     
-    
+  
+  # Implement to give the mode of training labels as a prediction for each data instance in X return labels
   def predict(self, X):
-    """
-    Implement to give the mode of training labels as a prediction for each data instance in X
-    return labels
-    """
-    
     labels = []
     for i in X:
       labels.append(self.mode)
@@ -184,17 +171,16 @@ class MajorityLabelClassifier():
 
 
 
-
-def learn_classifier(X_train, y_train, kernel):
-  """ learns a classifier from the input features and labels using the kernel function supplied
+"""
+  learns a classifier from the input features and labels using the kernel function supplied
   Inputs:
     X_train: scipy.sparse.csr.csr_matrix: sparse matrix of features, output of create_features()
     y_train: numpy.ndarray(int): dense binary vector of class labels, output of create_labels()
     kernel: str: kernel function to be used with classifier. [linear|poly|rbf|sigmoid]
   Outputs:
     sklearn.svm.classes.SVC: classifier learnt from data
-  """  
-  
+""" 
+def learn_classifier(X_train, y_train, kernel):  
   classifier = sklearn.svm.SVC(kernel = kernel)
   classifier.fit(X_train, y_train)
   return classifier
@@ -203,24 +189,22 @@ def learn_classifier(X_train, y_train, kernel):
 
 
 
-
-def evaluate_classifier(classifier, X_validation, y_validation):
-  """ evaluates a classifier based on a supplied validation data
+""" 
+  evaluates a classifier based on a supplied validation data
   Inputs:
     classifier: sklearn.svm.classes.SVC: classifer to evaluate
     X_validation: scipy.sparse.csr.csr_matrix: sparse matrix of features
     y_validation: numpy.ndarray(int): dense binary vector of class labels
   Outputs:
     double: accuracy of classifier on the validation data
-  """
-  
+"""
+def evaluate_classifier(classifier, X_validation, y_validation):
   predictions = classifier.predict(X_validation)
   accuracy = accuracy_score(y_validation, predictions) 
   return accuracy
  
 
-def best_model_selection(kf, X, y):
-  """
+"""
   Select the kernel giving best results using k-fold cross-validation.
   Other parameters should be left default.
   Input:
@@ -229,7 +213,8 @@ def best_model_selection(kf, X, y):
     y (array(int)): training labels
   Return:
     best_kernel (string)
-  """
+"""
+def best_model_selection(kf, X, y):
   best_kernel = 'linear'
   best_accuracy = 0
   
@@ -252,17 +237,16 @@ def best_model_selection(kf, X, y):
 
 
 
-
-def classify_tweets(tfidf, classifier, unlabeled_tweets):
-  """ predicts class labels for raw tweet text
+""" 
+  predicts class labels for raw tweet text
   Inputs:
     tfidf: sklearn.feature_extraction.text.TfidfVectorizer: the TfidfVectorizer object used on training data
     classifier: sklearn.svm.classes.SVC: classifier learnt
     unlabeled_tweets: pd.DataFrame: tweets read from tweets_test.csv
   Outputs:
     numpy.ndarray(int): dense binary vector of class labels for unlabeled tweets
-  """
-  
+"""
+def classify_tweets(tfidf, classifier, unlabeled_tweets): 
   tweets = pd.read_csv("tweets_train.csv", na_filter=False)
   train_tweets = process_all(tweets)
   tfidf, X = create_features(train_tweets, stopwords)
@@ -278,6 +262,38 @@ def classify_tweets(tfidf, classifier, unlabeled_tweets):
 
 
 
+
+def main():
+  tweets = pd.read_csv("tweets_train.csv", na_filter=False)
+  print('/nhead of tweets: ')
+  print(tweets.head())
+  
+  processed_tweets = process_all(tweets)
+  print('/nhead of processed tweets: ')
+  print(processed_tweets.head())
+  
+  (tfidf, X) = create_features(processed_tweets, stopwords)
+  print('/n shape of : ', X.shape)
+  
+  y = create_labels(processed_tweets)
+  print('/nsize of processed tweets labels: ', len(y))
+  
+  classifier = learn_classifier(X, y, 'linear')  
+  accuracy = evaluate_classifier(classifier, X, y)
+  print('/naccuracy of classifier: ', accuracy) 
+  # should give 0.956700196554515
+  
+  kf = sklearn.model_selection.KFold(n_splits=4, random_state=1, shuffle=True)
+  kf
+  
+  best_kernel = best_model_selection(kf, X, y)
+  print(best_kernel)
+
+
+
+
+if __name__ == '__main__':
+  main()
 
 
 
